9910de8359833f2d5ebd9388bec6e90dfe8b7341
maxc <maxc@coppeipartners.com>
2021-12-28 16:39:17 -0800

    tutorial 4

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -1,23 +1,20 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
-// import HomePage from "./HomePage";
+import HomePage from "./HomePage";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
   }
 
-//   render() {
-//     return (
-//       <div>
-//         <HomePage />
-//       </div>
-//     );
-//   }
-
   render() {
-      return <h1>Testing React Code</h1>
+    return (
+      <div>
+        <HomePage />
+      </div>
+    );
   }
+
 }
 
 const appDiv = document.getElementById("app");
