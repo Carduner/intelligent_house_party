83631f9aa55afe5459c6089d25d3b7fad99df80c
Max <maxcarduner@gmail.com>
2022-01-11 13:57:41 -0800

    tutorial 14; spotify api

--- music_controller/frontend/urls.py
+++ music_controller/frontend/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import index
 
+app_name = 'frontend' # for redirect for spotify views
+
 urlpatterns = [
-    path('', index),
+    path('', index, name=''), # for redirect in spotify views
     path('join', index),
     path('create', index),
     path('room/<str:roomCode>', index)
