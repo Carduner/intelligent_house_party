4305a355a48df4775f47589bc46ad31e206df2c3
Max <maxcarduner@gmail.com>
2022-01-13 12:31:09 -0800

    info page; functional components

--- music_controller/frontend/urls.py
+++ music_controller/frontend/urls.py
@@ -5,6 +5,7 @@ app_name = 'frontend' # for redirect for spotify views
 
 urlpatterns = [
     path('', index, name=''), # for redirect in spotify views
+    path('info', index),
     path('join', index),
     path('create', index),
     path('room/<str:roomCode>', index)
